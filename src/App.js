046182c9d19c0f02d546ea3f6bec0ab56e830841
Krunal Rajkotiya <krunal5071@gmail.com>
2021-04-08 22:47:28 +0530

    added:home page header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import React from 'react'
 import './App.css';
+import Header from './Header'
 
 function App() {
   return (
     // BEM
     <div className="app">
-     <h1> Hello </h1>
+     {/* Header */}
+      <Header/>
+     {/* HOME */}
     </div>
   );
 }
